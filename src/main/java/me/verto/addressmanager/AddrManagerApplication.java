c7711fa20ea9793f1d99f557385de8f929ede792
Everton Cardoso <verto@verto.me>
2015-11-20 20:28:26 -0200

    Initial commit

---
+++ src/main/java/me/verto/addressmanager/AddrManagerApplication.java
@@ -0,0 +1,12 @@
+package me.verto.addressmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddrManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddrManagerApplication.class, args);
+    }
+}
